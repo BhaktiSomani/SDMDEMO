d8cfc0d841deb5d87a9b224b0a9865da52d47662
Bhakti <bhaktisomani23@gmail.com>
2023-10-19 20:05:59 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "main.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	Student s = new Student();
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+class Student{
+	private: int id;
+	         String name;
+	public: 
+	       Student() {
+	       	id = 1;
+	       	name ="ram"
+		   }
+};
